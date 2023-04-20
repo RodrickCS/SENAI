8f4b6a6e9408a70332be9a6c87d66ded333e1ec4
RodrickCS <caetanorodrigo46@gmail.com>
2023-04-20 09:44:12 -0300

    pacientes completo (sem estilização)

--- 3DES/Aulas/ExercicioProdutividade/Pacientes/public/script.js
+++ 3DES/Aulas/ExercicioProdutividade/Pacientes/public/script.js
@@ -1,5 +1,75 @@
-function calculaIdade(anoDeNascimento) {
-  const anoAtual = new Date().getFullYear();
-  return anoAtual - anoDeNascimento;
+const uriDelete = "http://localhost:3000/delete/";
+const uriUpdatePaciente = "http://localhost:3000/update/"
+
+function excluirPaciente(id) {
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  fetch(uriDelete + id, options)
+    .then((response) => {
+      return response.status;
+    })
+    .then((data) => {
+      if (data === 204) {
+        window.location.reload();
+      } else {
+        alert("Houve um erro ao tentar excluir");
+      }
+    });
 }
-module.exports = calculaIdade;
+
+function openModal(btn) {
+  const row = btn.parentNode.parentNode;
+  const id = row.cells[0].textContent;
+  const nomeCompleto = row.cells[1].textContent;
+  const nascimento = row.cells[2].textContent;
+  const peso = row.cells[3].textContent;
+  const altura = row.cells[4].textContent;
+
+  document.querySelector("#modalId").value = id;
+  document.querySelector("#modalNome").value = nomeCompleto;
+  document.querySelector("#modalNascimento").value = nascimento;
+  document.querySelector("#modalPeso").value = peso;
+  document.querySelector("#modalAltura").value = altura;
+}
+
+function salvarAlteracoes() {
+  let id = document.querySelector("#modalId").value;
+  let nome = document.querySelector("#modalNome").value;
+  let nascimento = document.querySelector("#modalNascimento").value;
+  let peso = document.querySelector("#modalPeso").value;
+  let altura = document.querySelector("#modalAltura").value;
+
+  let form = {
+    nomeCompleto: nome,
+    nascimento: nascimento,
+    peso: peso,
+    altura: altura,
+  };
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(form),
+  };
+  fetch(uriUpdatePaciente + id, options)
+    .then((response) => {
+      return response.status;
+    })
+    .then((data) => {
+     if(data === 200) {
+      window.location.reload();
+     } else {
+      alert("Ocorreu um erro")
+     }
+    });
+}
+
+function closeModal() {
+  modal.classList.add("model");
+}
--- 3DES/Aulas/ExercicioProdutividade/Pacientes/src/controllers/paciente_controller.js
+++ 3DES/Aulas/ExercicioProdutividade/Pacientes/src/controllers/paciente_controller.js
@@ -2,14 +2,35 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const loadView = async (req, res) => {
-  let dados = await read();
+  let dados = await readVw();
   res.render("paciente", { dados });
+};
+
+const readVw = async (req, res) => {
+  try {
+    let dados = await prisma.$queryRaw`SELECT 
+    *, 
+    TIMESTAMPDIFF(YEAR, nascimento, CURDATE()) AS idade, 
+    ROUND(peso / POWER(altura, 2), 1) AS imc, 
+    CASE 
+      WHEN peso / POWER(altura, 2) < 18.5 THEN 'Abaixo do peso' 
+      WHEN peso / POWER(altura, 2) < 25 THEN 'Peso normal' 
+      WHEN peso / POWER(altura, 2) < 30 THEN 'Sobrepeso' 
+      WHEN peso / POWER(altura, 2) < 35 THEN 'Obesidade grau I' 
+      WHEN peso / POWER(altura, 2) < 40 THEN 'Obesidade grau II' 
+      ELSE 'Obesidade grau III ou mórbida' 
+    END AS diagnostico
+  FROM Paciente;`;    
+    return dados;
+  } catch (err) {
+    res.status(500).json(err).end();
+  }
 };
 
 const read = async (req, res) => {
   try {
     let paciente = await prisma.paciente.findMany();
-    return paciente ;
+    return paciente;
   } catch (err) {
     res.status(500).json(err).end();
   }
@@ -36,15 +57,23 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   try {
+    let parse = {
+      nomeCompleto: req.body.nomeCompleto,
+      nascimento: req.body.nascimento,
+      peso: Number(req.body.peso),
+      altura: Number(req.body.altura),
+    };
+
     let paciente = await prisma.paciente.update({
       where: {
-        id: req.params.id,
+        id: Number(req.params.id),
       },
-      data: req.body,
+      data: parse,
     });
     res.status(200).json(paciente).end();
   } catch (err) {
     res.status(500).json(err).end();
+    console.log(err);
   }
 };
 
@@ -52,12 +81,13 @@ const excluir = async (req, res) => {
   try {
     let paciente = await prisma.paciente.delete({
       where: {
-        id: req.params.id,
+        id: Number(req.params.id),
       },
     });
     res.status(204).json(paciente).end();
   } catch (err) {
     res.status(500).json(err).end();
+    console.error(err);
   }
 };
 
